46c0d9df5b9b5e6560d0dc7cb4273c2d3922fee6
David J Ottenheimer <dott92@gmail.com>
2020-06-11 13:55:18 -0400

    Update ottBari2020_root.m

--- MatlabScripts/HelperFunctions/ottBari2020_root.m
+++ MatlabScripts/HelperFunctions/ottBari2020_root.m
@@ -1,3 +1,3 @@
 function master_root = ottBari2020_root()
 % where are the Data and MatlabScripts folder located?
-master_root = 'C:\Users\bilal\Dropbox\RPE Manuscript copy\';
+master_root = 'C:\FolderContaining_Data_';
